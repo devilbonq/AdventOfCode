ed6bf29c0487b52da35b352333e4660c409168df
Jakub <jakub.boncler@gmail.com>
2018-12-13 00:12:40 +0100

    AoC - Day 2 - Part2

--- Day2/day2.py
+++ Day2/day2.py
@@ -8,11 +8,32 @@ def readFile(inputFile):
     return lines
 
 
+def functionB():
+    start = time.clock()
+    wordsList = readFile("input.txt")
+    word_lenght = len(wordsList[0])-1
+    for y in wordsList:
+        wordsList.pop(0)
+        for z in wordsList:
+            diffCounter = 0
+            for x in range(0, word_lenght):
+                if y[x] != z[x]:
+                    diffCounter += 1
+                    differencePosition = x
+                if diffCounter > 1:
+                    differencePosition == 0
+                    break
+            if diffCounter == 1:
+                common_chars = list(y)
+                common_chars.pop(differencePosition)
+
+    return ''.join(common_chars).rstrip("\n"), time.clock() - start
+
+
 def functionA():
     start = time.clock()
-    inputData = readFile("input.txt")
     doubles = triples = 0
-    for x in inputData:
+    for x in readFile("input.txt"):
         tempDouble = tempTriple = 0
         for key, value in collections.Counter(x.strip()).items():
             if value == 2:
@@ -29,3 +50,5 @@ def functionA():
 if __name__ == '__main__':
     resultA, timeA = functionA()
     print("Checksum: {}. Completed in {}.".format(resultA, timeA))
+    resultB, timeB = functionB()
+    print("Common chars between two similar (1 difference) strings: {}. Completed in {}.".format(resultB, timeB))
